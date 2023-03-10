969e40b91837c2c6ad7da467fe0c1f6a69ad3f77
Hermes Santos <hermessantos995@gmail.com>
2023-03-10 10:37:34 -0300

    feat: creating the first version

---
+++ scripts/script.js
@@ -0,0 +1,20 @@
+//Verify if the 'primary' id exists every one second
+let handleLoading = setInterval (()=>checkBodyLoad(), 1000)
+
+//if the 'primary' id existes it calls the click function
+function checkBodyLoad () {
+  let primary = document.getElementById('primary')
+  if (primary){
+    autoClick()
+  }
+}
+
+//click on the like button function
+function autoClick(){
+  let likeButton = document.getElementsByClassName('yt-spec-button-shape-next yt-spec-button-shape-next--tonal yt-spec-button-shape-next--mono yt-spec-button-shape-next--size-m yt-spec-button-shape-next--icon-leading yt-spec-button-shape-next--segmented-start ')[0]
+  //verifies if the like button is already clicked
+  if(likeButton.ariaPressed === 'false'){
+    //click action
+    likeButton.click()
+  }
+}
